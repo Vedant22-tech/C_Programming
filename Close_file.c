745f22caeb925854396c386a12f0e4eec860b142
Vedant Nandanwar <vedantnandanwar22@gmail.com>
2026-05-03 00:39:54 +0530

    Create Close_file.c

---
+++ Close_file.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<fcntl.h>           // File control
+#include<unistd.h>
+
+int main()
+{
+    int fd = 0;
+
+    fd = open("Marvellous.txt",O_RDWR);
+
+    if(fd == -1)
+    {
+        printf("Unable to open file");
+    }
+    else
+    {
+        printf("File gets opened with fd : %d\n",fd);
+    }
+    
+    close(fd);
+
+    return 0;
+}
